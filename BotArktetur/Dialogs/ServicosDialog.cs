999285ce126a29ab4f30cd95907dc3345d138a54
walldba <wallacedba@gmail.com>
2019-08-07 19:09:25 -0300

    Fluxo servicos + imagens thumbnail

--- BotArktetur/Dialogs/ServicosDialog.cs
+++ BotArktetur/Dialogs/ServicosDialog.cs
@@ -20,31 +20,40 @@ namespace BotArktetur.Dialogs
     {
         protected string conversationId;
 
+        public BotBody botBody;
+        public FraseologiaBot fraseologia;
+
+        public ServicosDialog()
+        {
+            botBody = CarrosselMenu.LerArquivoJsonBot();
+            fraseologia = CarrosselMenu.LerFraseologia();
+        }
         public async Task StartAsync(IDialogContext context)
         {
             // carregar os itens da página sobre
-            await CarregaCarrosselServicos(context);
+            await CarregaTextoServicos(context);
 
             //context.Wait(MessageReceivedAsync);
         }
 
-        private async Task CarregaCarrosselServicos(IDialogContext context)
+        private async Task CarregaTextoServicos(IDialogContext context)
         {
-            var botBody = CarrosselMenu.LerArquivoJsonBot().Dialogs.Servicos;
-            var fraseologia = CarrosselMenu.LerFraseologia().FraseologiaSaudacao;
-
-            await context.PostAsync(botBody.TextoServico);
+            await context.PostAsync(botBody.Dialogs.Servicos.TextoServico);
             await Task.Delay(800);
 
-            var reply = context.MakeMessage();
-            reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
-            reply.Attachments = MontarMenuServicos(botBody.ListaServico);
-            await context.PostAsync(reply);
+            var carrossel = context.MakeMessage();
+            carrossel.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+            carrossel.Attachments = MontarMenu();
+            await context.PostAsync(carrossel);
+            await Task.Delay(800);
 
-            context.Wait(VoltarMenuServicos);
+            await context.PostAsync(botBody.Dialogs.Sobre.OpcoesVoltar);
+            await Task.Delay(800);
+
+            context.Wait(VoltarMenuSobre);
         }
 
-        public static IList<Attachment> MontarMenuServicos(List<Servico> listaServicos)
+        public static IList<Attachment> MontarMenu()
         {
             // Mostra o item escolhido pelo usuário
             //ActionTypes.ImBack
@@ -54,27 +63,80 @@ namespace BotArktetur.Dialogs
             CarrosselMenu card = new CarrosselMenu();
             List<ItemCarrosselSemBotao> listaItensMenu = new List<ItemCarrosselSemBotao>();
 
-            foreach(Servico servico in listaServicos)
+            listaItensMenu.Add(new ItemCarrosselSemBotao()
             {
-                listaItensMenu.Add(new ItemCarrosselSemBotao()
-                {
-                    Titulo = servico.Nome,
-                    SubTitulo = servico.Descricao,
-                    Imagem = new CardImage(url: "https://raw.githubusercontent.com/walldba/JL-Project/master/SkyCobranca_Imagens/alegarPagamento.png")
-                });
-            }
+                Texto = "ARQUITETURA DE SOFTWARE",
+                SubTitulo = "Desenvolvemos arquiteturas de software de alto nível para o seu negócio.",
+                Imagem = new CardImage(url: "https://raw.githubusercontent.com/cassiogudim/BotArktetur/master/BotArktetur/Images/Carrossel/Sobre.png"),
+            });
+            listaItensMenu.Add(new ItemCarrosselSemBotao()
+            {
+                Texto = "CLOUD COMPUTING",
+                SubTitulo = "Desenvolvemos sua arquitetura e levamos seu negócio para a nuvem.",
+                Imagem = new CardImage(url: "https://raw.githubusercontent.com/cassiogudim/BotArktetur/master/BotArktetur/Images/Carrossel/Servicos.png"),
+            });
+            listaItensMenu.Add(new ItemCarrosselSemBotao()
+            {
+                Texto = "INTELIGENT BOT SERVICES",
+                SubTitulo = "Criamos, conectamos, implantamos e gerenciamos bots inteligentes para interagir naturalmente com seus clientes.",
+                Imagem = new CardImage(url: "https://raw.githubusercontent.com/cassiogudim/BotArktetur/master/BotArktetur/Images/Carrossel/Clientes.png"),
+            });
+            listaItensMenu.Add(new ItemCarrosselSemBotao()
+            {
+                Texto = "ARQUITETURA DE BANCO DE DADOS",
+                SubTitulo = "Garanta mais segurança e consistência dos seus dados com um banco de dados bem estruturado.",
+                Imagem = new CardImage(url: "https://raw.githubusercontent.com/cassiogudim/BotArktetur/master/BotArktetur/Images/Carrossel/Founders.png"),
+            });
+            listaItensMenu.Add(new ItemCarrosselSemBotao()
+            {
+                Texto = "DESENVOLVIMENTO BACK-END, FRONT-END E FULLSTACK",
+                SubTitulo = "Possuímos uma equipe especializada em desenvolvimento de software\r\nque trabalha através de métodos ágeis. Entregamos seu software com mais agilidade, padrão e por um menor custo.",
+                Imagem = new CardImage(url: "https://raw.githubusercontent.com/cassiogudim/BotArktetur/master/BotArktetur/Images/Carrossel/Parceiros.png"),
+            });
+            listaItensMenu.Add(new ItemCarrosselSemBotao()
+            {
+                Texto = "CONSULTORIA EM NIST 800-53 e 171",
+                SubTitulo = "",
+                Imagem = new CardImage(url: "https://raw.githubusercontent.com/walldba/JL-Project/master/SkyCobranca_Imagens/sairSky.png"),
+            });
+            listaItensMenu.Add(new ItemCarrosselSemBotao()
+            {
+                Texto = "CONSULTORIA EM LGPD",
+                SubTitulo = "A Falta de controle dos dados pode resultar em sanções duras que, atribuídas ao não respeito da LGPD e da GDPR, podem levar empresas a proibição total ou parcial de suas atividades baseadas em dados, ocasionando também multas e publicações oficiais das empresas que forem constatadas como não confiáveis para a atuação.",
+                Imagem = new CardImage(url: "https://raw.githubusercontent.com/walldba/JL-Project/master/SkyCobranca_Imagens/sairSky.png"),
+            });
+            listaItensMenu.Add(new ItemCarrosselSemBotao()
+            {
+                Texto = "CONSULTORIA EM PCI-DSS",
+                SubTitulo = "O PCI DSS está composto por um conjunto de requerimentos e procedimentos de segurança cujo objetivo é proteger as informações pessoais dos titulares de cartão e, portanto, reduzir o risco de roubo de dados de cartão ou fraude.\r\n\r\nO PCI DSS é aplicado a qualquer negócio que processe ou transmita dados dos portadores de cartões.\r\n\r\nAjudamos a preparar a sua aplicação e infraestrutura para a certificação.",
+                Imagem = new CardImage(url: "https://raw.githubusercontent.com/walldba/JL-Project/master/SkyCobranca_Imagens/sairSky.png"),
+            });
+            listaItensMenu.Add(new ItemCarrosselSemBotao()
+            {
+                Texto = "CONSULTORIA EM ISO 27001",
+                SubTitulo = "ABNT ISO/IEC 27001 auxilia as empresas na implantação de um sistema de gestão da segurança da informação. Por ser orientada ao negócio, não apenas à tecnologia, esta norma provê orientações para criação dos processos e qualificação das pessoas que utilizam as tecnologias responsáveis por permitir às empresas alcançarem seus objetivos socioeconômicos.",
+                Imagem = new CardImage(url: "https://raw.githubusercontent.com/walldba/JL-Project/master/SkyCobranca_Imagens/sairSky.png"),
+            });
 
             return card.GerarCarrosselCompletoSemBotao(listaItensMenu);
         }
 
-        private async Task VoltarMenuServicos(IDialogContext context, IAwaitable<IMessageActivity> messageActivity)
+        private async Task VoltarMenuSobre(IDialogContext context, IAwaitable<IMessageActivity> messageActivity)
         {
             try
             {
                 conversationId = context.Activity.Conversation.Id;
                 var message = await messageActivity;
                 var textoDigitado = message.Text.Trim();
-             
+
+                if (textoDigitado.Contains("sair"))
+                {
+                    context.Done(true);
+                }
+                else
+                {
+                    context.Call(new MenuPrincipalDialog(), MessageResumeAfter);
+                }
                 //recupera informação
                 //info = context.PrivateConversationData.GetValueOrDefault(conversationId, new InfoConversation());
 
@@ -92,9 +154,9 @@ namespace BotArktetur.Dialogs
 
                 string json = JsonConvert.SerializeObject(error, Formatting.Indented);
 
-                await context.PostAsync("Erro método MessageReceivedAsync do diálogo ServicosDialog: " + json);
+                await context.PostAsync("Erro método MessageReceivedAsync do diálogo GreetingDialog: " + json);
             }
-        }        
+        }
 
         public async Task MessageResumeAfter(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
